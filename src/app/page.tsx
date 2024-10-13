b65df4e40f0d09453163b4cfb6c37ff006cd365a
Kevon Young-Reaves <youngreavesk@gmail.com>
2024-10-13 10:43:09 -0500

    Add force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "../server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/PQH5dZBWrEM3Yr1cXKADqKgwdh2SGzO8Lx5BMEWPfcNX9lm6",
   "https://utfs.io/f/PQH5dZBWrEM3iIti1LXWDbP1o4GjHkOMALaQfsC96xvdNRSt",
