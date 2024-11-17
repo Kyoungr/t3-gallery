0f1c388e37173ec8c58ec1103aea5e15385be2e4
Kevon Young-Reaves <youngreavesk@gmail.com>
2024-11-17 02:30:45 -0300

    Add simple upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,32 +1,18 @@
 "use client";
 
-import {
-  SignedIn,
-  SignedOut,
-  SignIn,
-  SignInButton,
-  UserButton,
-} from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SimpleUploadButton } from "~/components/simple-upload-button";
 
 export function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="text-x1 flex w-full items-center justify-between border-b p-4 font-semibold">
       <div>Gallery</div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
