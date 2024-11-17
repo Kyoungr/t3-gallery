7c21a44fff15977ce60c6ce1adc65c166c0a8479
Kevon Young-Reaves <youngreavesk@gmail.com>
2024-11-17 00:59:40 -0300

    include grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-4 p-4">
       <div className="h-full w-full text-center text-2xl">
         Welcome to my Gallery
       </div>
